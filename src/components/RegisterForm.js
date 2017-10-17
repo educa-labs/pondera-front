298f83ecd76e93630ca2b95408fbe861141e889b
Ivan Wolf <ivanwolf15@gmail.com>
2017-10-16 23:49:01 -0300

    Home and register

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Panel from 'muicss/lib/react/panel';
 import Form from 'muicss/lib/react/form';
 import Input from 'muicss/lib/react/input';
 
-const LoginForm = () => (
+const RegisterForm = ({ onSubmit }) => (
   <Panel>
-    <Form>
+    <Form onSubmit={onSubmit}>
+      <legend>Regístrate</legend>
       <Input label="Correo electrónico" />
       <Input label="Contraseña" />
     </Form>
   </Panel>
 );
 
-export default LoginForm;
+RegisterForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default RegisterForm;
 
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'muicss/lib/react/container';
 import Row from 'muicss/lib/react/row';
@@ -6,16 +6,61 @@ import Col from 'muicss/lib/react/col';
 import RegisterForm from '../components/RegisterForm';
 
 
-const Home = () => (
-  <Container fluid className="page mui--bg-primary">
-    <Row>
-      <Col xs={12} md={4} md-offset={4}>
-        <h1>Regístrate</h1>
-        <RegisterForm />
-        <div className="mui--text-body1">¿Ya tienes una cuenta? <Link to="login">Ingresa.</Link> </div>
-      </Col>
-    </Row>
-  </Container>
-);
+const AnimatedBanner = ({ frame }) => ([
+  <div key="0" className="from-left mui--text-display2">
+    <h2>
+      Pondera
+    </h2>
+  </div>,
+  <div key="1" className={`${frame === 1 ? 'from-right' : ''} mui--text-display2`}>
+    <div className="car-photo">
+      {frame === 1 && frame}
+    </div>
+  </div>,
+]);
+
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      frame: 0,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(this.getNextFrame.bind(this), 1000);
+  }
+
+  getNextFrame() {
+    this.setState(prevState => ({
+      frame: prevState.frame + 1,
+    }));
+  }
+
+  handleSubmit() {
+    console.log(...this.state);
+  }
+
+  render() {
+    return (
+      <Container fluid className="page mui--bg-primary">
+        <Row>
+          <Col xs={12} md={4} md-offset={4}>
+            <AnimatedBanner frame={this.state.frame} />
+            <RegisterForm
+              email={this.state.email}
+              password={this.state.password}
+              onSubmit={this.handleSubmit}
+            />
+            <div className="mui--text-body1">¿Ya tienes una cuenta? <Link to="login">Ingresa.</Link> </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}  
 
 export default Home;
