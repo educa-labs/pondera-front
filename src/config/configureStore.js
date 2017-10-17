322b73e48e0fc90600937d363b1e4a15e1edb71b
Ivan Wolf <ivanwolf15@gmail.com>
2017-10-16 23:49:12 -0300

    endabled hot reload on reducer

--- src/config/configureStore.js
+++ src/config/configureStore.js
@@ -1,13 +1,21 @@
+/* eslint-disable global-require */
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import reducer from '../reducers';
 
 function configureStore() {
-  return createStore(
+  const store = createStore(
     reducer,
     composeWithDevTools(applyMiddleware(thunkMiddleware)),
   );
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers');
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+  return store;
 }
 
 export default configureStore;
