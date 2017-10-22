ebc541a792654f2d6a66d5c8464e5e9d8a5b3c89
Ivan Wolf <ivanwolf15@gmail.com>
2017-10-21 22:33:57 -0300

    added navigationbar

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../../assets/svg/logo-pondera.svg';
+
+const NavigationBar = () => (
+  <nav>
+    <Logo width={80} height={35} />
+  </nav>
+);
+
+export default NavigationBar;
