54eadea4c368d53c4954ab814dc60f6d8c2da940
Ivan Wolf <ivanwolf15@gmail.com>
2017-10-21 21:43:14 -0300

    new component

---
+++ src/components/Layout/ScrollScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Motion, spring } from 'react-motion';
+
+const ScrollScreen = ({ index, children }) => {
+  const renderScreen = child => (
+    <div className="screen">
+      {child}
+    </div>
+  );
+  return (
+    <Motion style={{ y: spring(index * 100) }}>
+      {({ y }) => (
+        <div className="scroll-screen">
+          <div style={{ transform: `translateY(-${y}vh)` }}>
+            {React.Children.map(children, renderScreen)}
+          </div>
+        </div>
+      )}
+    </Motion>
+  );
+};
+
+ScrollScreen.propTypes = {
+  index: PropTypes.number.isRequired,
+  children: PropTypes.oneOf([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export default ScrollScreen;
+
