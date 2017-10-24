56181dc7f386cc48f92cb93a222bce8eb400e669
Ivan Wolf <ivanwolf15@gmail.com>
2017-10-24 16:25:08 -0300

    Create connectForm
    This HOC whil bind the component with the reducer.  The names must match

---
+++ src/hoc/connectForm.js
@@ -0,0 +1,31 @@
+import { connect } from 'react-redux';
+import { logChange, validFormSelector, submitForm } from '../redux/forms';
+
+/*
+  handleSubmit: Funcion que recive como parametro una
+  funcion que será ejecutada con los valores del formulario
+*/
+
+export default function (Component, formName, validator, fields) {
+  const mapStateToProps = state => ({
+    values: state[formName].values,
+    errors: state[formName].errors,
+    isValidForm: validFormSelector(state[formName]),
+  });
+  const mapDispatchToProps = dispatch => ({
+    logChange: field => (
+      ev => dispatch(logChange(formName, field, ev.target.value))
+    ),
+    submitHandler: handleSubmit => (
+      (ev) => {
+        ev.preventDefault();
+        dispatch(submitForm(formName, handleSubmit, validator, fields));
+      }
+    ),
+  });
+
+
+  const connectForm = connect(mapStateToProps, mapDispatchToProps)(Component);
+  return connectForm;
+}
+
