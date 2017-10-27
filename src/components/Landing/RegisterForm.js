9f466c237849d8cd899056b0cd2e2e9b63a72c54
Ivan Wolf <ivanwolf15@gmail.com>
2017-10-27 11:41:13 -0300

    Fixed link not being imported

--- src/components/Landing/RegisterForm.js
+++ src/components/Landing/RegisterForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Form from 'muicss/lib/react/form';
 import Button from 'muicss/lib/react/button';
+import { Link } from 'react-router-dom';
 import TextInput from '../Inputs/TextInput';
 import wrapCard from '../../hoc/wrapCard';
 import connectForm from '../../hoc/connectForm';
