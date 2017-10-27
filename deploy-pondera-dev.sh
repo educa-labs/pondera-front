4fbd454dc1afede829aefa52befdf4d93bca6545
Felipe Haase <fahaase@uc.cl>
2017-10-27 12:16:28 -0300

    fix on deploy dev

--- deploy-pondera-dev.sh
+++ deploy-pondera-dev.sh
@@ -1,9 +1,9 @@
-echo "Chupalo Ivan"
+echo "Comenzando deploy Pondera.cl"
 
 npm install
 
 npm run build
 
-s3cmd --access_key=ENV['educalabs_aws_key'] --secret_key=ENV['educalabs_aws_secret'] sync build/* s3://beta.pondera.cl
+s3cmd --access_key=ENV['educalabs_aws_key'] --secret_key=ENV['educalabs_aws_secret'] sync dist/* s3://beta.pondera.cl
 
 echo "Deploy listo ctm"
