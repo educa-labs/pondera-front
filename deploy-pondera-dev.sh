80b0fc5c4bbdb72a3f0cce16778d9f5c7a1d2c18
Ivan Wolf <ivanwolf15@gmail.com>
2017-10-27 14:38:27 -0300

    remove bad script

--- deploy-pondera-dev.sh
+++
@@ -1,9 +0,0 @@
-echo "Chupalo Ivan"
-
-npm install
-
-npm run build
-
-s3cmd --access_key=ENV['educalabs_aws_key'] --secret_key=ENV['educalabs_aws_secret'] sync build/* s3://beta.pondera.cl
-
-echo "Deploy listo ctm"
