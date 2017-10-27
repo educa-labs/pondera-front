7da947c64a6eeed6700940f4240afb6f03915204
Ivan Wolf <ivanwolf15@gmail.com>
2017-10-27 12:56:28 -0300

    add deply script

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -1,9 +1,7 @@
-echo "Comenzando deploy Pondera.cl"
-
-npm install
-
-npm run build
-
+echo "Starting deploy Pondera.cl"
+source .env
+yarn install
+yarn build
 s3cmd --access_key=$educalabs_aws_key --secret_key=$educalabs_aws_secret sync dist/* s3://beta.pondera.cl
 
-echo "Deploy listo ctm"
+echo "Deploy readt: beta.pondera.cl "
