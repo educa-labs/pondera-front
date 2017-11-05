f3d5bdb50ddeae129aed26df6ff5ec46390d88af
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-04 22:34:00 -0300

    new routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Landing from './Landing';
-import StepTwo from './StepTwo';
 import Simula from './Simula';
 import Login from './Login';
 import Terms from '../components/Terms/Terms';
 import Contacto from '../components/Terms/Contacto';
 
 const App = () => (
-  <Switch key="1">
-    <Route exact path="/" component={Landing} />
-    <Route path="/step-two" component={StepTwo} />
+  <Switch>
     <Route path="/simula" component={Simula} />
     <Route path="/login" component={Login} />
     <Route path="/terms" component={Terms} />
     <Route path="/contacto" component={Contacto} />
+    <Route path="/" component={Landing} />
   </Switch>
 );
 
