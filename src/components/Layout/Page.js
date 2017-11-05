e9370c6b269c0d71d2d58d1ce244a1f06a713be1
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-04 22:33:46 -0300

    page now has a nav for default

--- src/components/Layout/Page.js
+++ src/components/Layout/Page.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import NavigationBar from '../NavigationBar/NavigationBar';
 
 const Page = props => (
   <div className="page">
+    <NavigationBar key="0" />
     <div className={`orange-banner ${props.largeBanner ? 'orange-banner--large' : ''}`} />
     <div className="page-content">
       {props.children}
