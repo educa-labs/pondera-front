adc22400b2b89d8d673a4db8a2450fea053f6652
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-04 22:32:05 -0300

    new packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
     "rules": {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
         "jsx-a11y/anchor-is-valid": 0,
+        "react/no-children-prop": 0,
     }
 };
