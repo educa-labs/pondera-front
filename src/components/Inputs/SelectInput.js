f563a8535a4b844382e1a646159a0f4535b8d6bc
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-23 20:28:06 -0300

    default option

--- src/components/Inputs/SelectInput.js
+++ src/components/Inputs/SelectInput.js
@@ -3,15 +3,22 @@ import PropTypes from 'prop-types';
 import Option from 'muicss/lib/react/option';
 import Select from 'muicss/lib/react/select';
 
-const SelectInput = ({ options, ...props }) => (
-  <Select {...props}>
-    {options.map(opt => (
-      <Option key={opt.id} value={opt.id} label={opt.title} />
-    ))}
-  </Select>
-);
+const SelectInput = ({ options, placeholder, ...props }) => {
+  const defaultOption = {
+    id: '',
+    title: placeholder,
+  };
+  return (
+    <Select {...props}>
+      {[defaultOption, ...options].map(opt => (
+        <Option key={opt.id} value={opt.id} label={opt.title} />
+      ))}
+    </Select>
+  );
+};
 
 SelectInput.propTypes = {
+  placeholder: PropTypes.string.isRequired,
   options: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
