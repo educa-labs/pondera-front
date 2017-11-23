bd33938d65c1a41ff707886a6ff940b6867b8612
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-23 18:10:48 -0300

    minor refactor

--- src/components/Pondera/PonderaForm.js
+++ src/components/Pondera/PonderaForm.js
@@ -25,122 +25,121 @@ const PonderaForm = ({
   selectedTest,
   setHistoryRef,
   setScienceRef,
-}) => {
-  return (
-    <Form onSubmit={onSubmit}>
-      <legend>Ponderar</legend>
-      <div className="mui--text-subhead">
-        Ingresa tus puntajes y carrera
-      </div>
-      <Container>
-        <Row>
-          <Col xs={6} className="padding-col">
-            <Field name="nem">
-              <TextInput
-                label="NEM"
-                floatingLabel
-                type="number"
-              />
-            </Field>
-            <Field name="language">
-              <TextInput
-                label="Leng"
-                floatingLabel
-                type="number"
-              />
-            </Field>
-            <RadioWrapper
-              onSelect={onSelectTest}
-              selected={selectedTest}
-              id={HISTORY}
-            >
-              <Field name="history">
-                <TextInput
-                  label="Hist"
-                  floatingLabel
-                  type="number"
-                  setRef={setHistoryRef}
-                />
-              </Field>
-            </RadioWrapper>
-          </Col>
-          <Col xs={6} className="padding-col">
-            <Field name="ranking">
-              <TextInput
-                label="Rank"
-                floatingLabel
-                type="number"
-              />
-            </Field>
-            <Field name="math">
-              <TextInput
-                label="Mate"
-                floatingLabel
-                type="number"
-              />
-            </Field>
-            <RadioWrapper
-              onSelect={onSelectTest}
-              selected={selectedTest}
-              id={SCIENCE}
-            >
-              <Field name="science">
-                <TextInput
-                  label="Cien"
-                  floatingLabel
-                  type="number"
-                  setRef={setScienceRef}
-                />
-              </Field>
-            </RadioWrapper>
-          </Col>
-        </Row>
-      </Container>
-      <LoadingWrapper loading={univs === null}>
-        {() => (
-          <div>
-            <SelectInput
-              label="Universidad"
-              options={univs}
-              onChange={onUnivChange}
-            />
-            <LoadingWrapper loading={isLoading}>
-              {() => (
-                <SelectInput
-                  label="Carrera"
-                  options={careers}
-                />
-              )}
-            </LoadingWrapper>
-          </div>
-        )}
-      </LoadingWrapper>
+}) => (
+  <Form onSubmit={onSubmit}>
+    <legend>Ponderar</legend>
+    <div className="mui--text-subhead">
+      Ingresa tus puntajes y carrera
+    </div>
+    <Container>
       <Row>
-        <Col xs={6}>
-          <Button
-            color="primary"
-            type="button"
-            className="btn--fullwidth"
-            variant="flat"
-            onClick={resetForm}
+        <Col xs={6} className="padding-col">
+          <Field name="nem">
+            <TextInput
+              label="NEM"
+              floatingLabel
+              type="number"
+            />
+          </Field>
+          <Field name="language">
+            <TextInput
+              label="Leng"
+              floatingLabel
+              type="number"
+            />
+          </Field>
+          <RadioWrapper
+            onSelect={onSelectTest}
+            selected={selectedTest}
+            id={HISTORY}
           >
-            Reestablecer
-          </Button>
+            <Field name="history">
+              <TextInput
+                label="Hist"
+                floatingLabel
+                type="number"
+                setRef={setHistoryRef}
+              />
+            </Field>
+          </RadioWrapper>
         </Col>
-        <Col xs={6}>
-          <Button
-            color="primary"
-            type="submit"
-            className="btn--fullwidth"
-            variant="raised"
+        <Col xs={6} className="padding-col">
+          <Field name="ranking">
+            <TextInput
+              label="Rank"
+              floatingLabel
+              type="number"
+            />
+          </Field>
+          <Field name="math">
+            <TextInput
+              label="Mate"
+              floatingLabel
+              type="number"
+            />
+          </Field>
+          <RadioWrapper
+            onSelect={onSelectTest}
+            selected={selectedTest}
+            id={SCIENCE}
           >
-            Calcular
-          </Button>
+            <Field name="science">
+              <TextInput
+                label="Cien"
+                floatingLabel
+                type="number"
+                setRef={setScienceRef}
+              />
+            </Field>
+          </RadioWrapper>
         </Col>
       </Row>
-    </Form>
-  );
-};
+    </Container>
+    <LoadingWrapper loading={univs === null}>
+      {() => (
+        <div>
+          <SelectInput
+            label="Universidad"
+            options={univs}
+            onChange={onUnivChange}
+          />
+          <LoadingWrapper loading={isLoading}>
+            {() => (
+              <SelectInput
+                label="Carrera"
+                options={careers}
+              />
+            )}
+          </LoadingWrapper>
+        </div>
+      )}
+    </LoadingWrapper>
+    <Row>
+      <Col xs={6}>
+        <Button
+          color="primary"
+          type="button"
+          className="btn--fullwidth"
+          variant="flat"
+          onClick={resetForm}
+        >
+          Reestablecer
+        </Button>
+      </Col>
+      <Col xs={6}>
+        <Button
+          color="primary"
+          type="submit"
+          className="btn--fullwidth"
+          variant="raised"
+        >
+          Calcular
+        </Button>
+      </Col>
+    </Row>
+  </Form>
+);
+
 
 PonderaForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
@@ -152,6 +151,9 @@ PonderaForm.propTypes = {
   onUnivChange: PropTypes.func.isRequired,
   onSelectTest: PropTypes.func.isRequired,
   selectedTest: PropTypes.string.isRequired,
+  resetForm: PropTypes.func.isRequired,
+  setHistoryRef: PropTypes.func.isRequired,
+  setScienceRef: PropTypes.func.isRequired,
 };
 
 
