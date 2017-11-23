dd601db5311220160f39dd2bc943a89c01588a67
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-23 20:51:27 -0300

    Spinner white

--- src/components/Other/Loading.js
+++ src/components/Other/Loading.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Spinner from '../../assets/svg/spinner.svg';
 
-const Loading = ({ size }) => (
+const Loading = ({ size, white }) => (
   <div className="loading-wrapper">
-    <Spinner width={size} height={size} />
+    <svg className="spinner" width={`${size}px`} height={`${size}px`} viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+      <circle className={`path ${white ? 'path-white' : '' }`} fill="none" strokeLinecap="round" cx="33" cy="33" r="30" />
+    </svg>
   </div>
 );
 
 Loading.defaultProps = {
   size: 30,
+  white: false,
 };
 
 Loading.propTypes = {
   size: PropTypes.number,
+  white: PropTypes.bool,
 };
 
 export default Loading;
--- src/components/Other/LoadingWrapper.js
+++ src/components/Other/LoadingWrapper.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Loading from './Loading';
 
-const LoadingWrapper = ({ loading, children }) => (
-  loading ? <Loading /> : children()
+const LoadingWrapper = ({ loading, children, white }) => (
+  loading ? <Loading white={white} /> : children()
 );
 
 export default LoadingWrapper;
