2b50e0d46b7c7baa28ca9251be9c344309c00e86
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-23 23:00:30 -0300

    Ubuntu script

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -2,6 +2,6 @@ echo "Starting deploy Pondera.cl"
 source .env
 yarn install
 yarn build
-s3cmd --access_key=$educalabs_aws_key --secret_key=$educalabs_aws_secret sync dist/* s3://beta.pondera.cl
+s3cmd sync dist/* s3://beta.pondera.cl
 
 echo "Deploy readt: beta.pondera.cl "
