2c38d1d418de1059b91313d66d36e0d3f467486c
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-23 22:32:14 -0300

    new spinner

--- src/components/Other/Loading.js
+++ src/components/Other/Loading.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Loading = ({ size, white }) => (
   <div className="loading-wrapper">
-    <svg className="spinner" width={`${size}px`} height={`${size}px`} viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+    <svg className="spinner-circle" width={`${size}px`} height={`${size}px`} viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
       <circle className={`path ${white ? 'path-white' : '' }`} fill="none" strokeLinecap="round" cx="33" cy="33" r="30" />
     </svg>
   </div>
