b5b84973b05ca95b36bf4fdeedba01397b400b95
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-25 16:41:19 -0300

    New spinner

--- src/components/Other/LoadingWrapper.js
+++ src/components/Other/LoadingWrapper.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Loading from './Loading';
+import Spinner from './Spinner';
 
 const LoadingWrapper = ({ loading, children, white }) => (
-  loading ? <Loading white={white} /> : children()
+  loading ? (
+    <div className="loading-wrapper">
+      <Spinner white={white} />
+    </div>
+  ) : children()
 );
 
 export default LoadingWrapper;
