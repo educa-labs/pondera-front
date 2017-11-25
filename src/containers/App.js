7fb91939df896ae33bc0c804f1e42677bc0bd084
Ivan Wolf <ivanwolf15@gmail.com>
2017-11-25 19:50:34 -0300

    Ready screen

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route, withRouter } from 'react-router-dom';
-import ProtectedRoute from '../hoc/ProtectedRoute';
-import { isLogged } from '../redux/session';
 import { setPromptEvent } from '../redux/promptEvent';
 import Landing from './Landing';
 import Simula from './Simula';
 import Login from './Login';
 import Terms from '../components/Terms/Terms';
+import Ready from '../components/Terms/Ready';
 import Contacto from '../components/Terms/Contacto';
 import Background from '../components/Layout/BackGround';
-import Test from './Test';
 
 class App extends React.Component {
   componentDidMount() {
@@ -22,35 +20,24 @@ class App extends React.Component {
     });
   }
 
-  shouldComponentUpdate(nextProps) {
-    if (nextProps.delay !== this.props.delay) {
-      if (nextProps.delay) return false;
-    }
-    return true;
-  }
-
   render() {
-    const { isLogged, delay } = this.props;
     return (
       <div>
         <Background />
         <Switch>
-          <Route path="/simula"component={Simula}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/test" component={Test}/>
-          <Route path="/terms" component={Terms}/>
-          <Route path="/contacto" component={Contacto}/>
-          <Route path="/" component={Landing}/>
+          <Route path="/simula"component={Simula} />
+          <Route path="/login" component={Login} />
+          <Route path="/terms" component={Terms} />
+          <Route path="/contacto" component={Contacto} />
+          <Route path="/ready" component={Ready} />
+          <Route path="/" component={Landing} />
         </Switch>
       </div>
     );
   }
 }
 
-const connectedApp = connect(state => ({
-  isLogged: isLogged(state),
-  delay: state.delayAnimation,
-}))(App);
+const connectedApp = connect(null)(App);
 
 // export default App;
 export default withRouter(connectedApp);
