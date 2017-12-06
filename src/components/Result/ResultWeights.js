ce2dd0af0ba74a0a473da2bcdc99f6ab3114a4f3
Ivan Wolf <ivanwolf15@gmail.com>
2017-12-06 18:04:42 -0300

    supports boths tests

--- src/components/Result/ResultWeights.js
+++ src/components/Result/ResultWeights.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import Container from 'muicss/lib/react/container';
 
-const subjects = [
-  { value: 'NEM', label: 'NEM' },
-  { value: 'ranking', label: 'Rank' },
-  { value: 'language', label: 'Lang' },
-  { value: 'math', label: 'Mate' },
-  { value: 'history', label: 'Hist' },
-];
 
 const ResultWeights = ({ result }) => {
   if (result === null) return null;
+  const subjects = [
+    { value: 'NEM', label: 'NEM' },
+    { value: 'ranking', label: 'Rank' },
+    { value: 'language', label: 'Lang' },
+    { value: 'math', label: 'Mate' },
+  ];
+  const { history, science } = result.weights;
+  if (history > 0) {
+    if (science > 0) {
+      subjects.push({
+        value: 'history',
+        label: 'Hist/Cien',
+      });
+    } else {
+      subjects.push({
+        value: 'history',
+        label: 'Hist',
+      });
+    }
+  } else {
+    subjects.push({
+      value: 'science',
+      label: 'Cien',
+    });
+  }
   const renderWeight = ({ value, label }, index) => (
     <div className="result-item" key={index}>
       <div className="result-item-label">{label}</div>
