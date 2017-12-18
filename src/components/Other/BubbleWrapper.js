00c42a3a94bb4701e077f7971763dab2f84ec571
Ivan Wolf <ivanwolf15@gmail.com>
2017-12-18 12:40:47 -0300

    no bubble on desk

--- src/components/Other/BubbleWrapper.js
+++ src/components/Other/BubbleWrapper.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import Bubble from './Bubble';
 
-const BubbleWrapper = ({ trigger, children }) => (
-  <div className="bubble-wrapper">
-    <Bubble show={!trigger} />
+const BubbleWrapper = ({ trigger, children }) => ([
+  <MediaQuery key="0" maxDeviceWidth={1224}>
+    <div className="bubble-wrapper">
+      <Bubble show={!trigger} />
+      {children}
+    </div>
+  </MediaQuery>,
+  <MediaQuery key="1" minDeviceWidth={1224}>
     {children}
-  </div>
-);
+  </MediaQuery>,
+]);
 
 export default BubbleWrapper;
 
