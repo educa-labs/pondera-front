5bcef5b834ee177a49c8f9bca5b79d779a4a1c10
Ivan Wolf <ivanwolf15@gmail.com>
2017-12-18 15:16:15 -0300

    hot fixes

--- src/components/Terms/Ready.js
+++ src/components/Terms/Ready.js
@@ -6,7 +6,7 @@ const content = ({ history }) => (
     <legend>¡Listo!</legend>
     <p>Ya estás registrado en <strong>www.pondera.cl</strong>. Para participar deberás iniciar sesión y ponderar con tus puntajes PSU a partir del 26 de Diciembre a las 8:00.</p>
     <br />
-    <p> Los ganadores se anunciarán el 3 de enero en el grupo PSU 2017 de Facebook.</p>
+    <p> Los ganadores se anunciarán el 22 de enero en el grupo PSU 2017 de Facebook.</p>
     <br />
     <p>¡Mucha suerte!</p>
   </div>
